99283702d52d918b10bd47ec4bf563e3245f6509
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-11-24 14:05:04 +0530

    Add files via upload

---
+++ AbstractExample.java
@@ -0,0 +1,17 @@
+abstract class Person
+{
+abstract void Show();
+}
+class Student extends PErson
+{
+void show()
+{
+}
+class AbstractExample 
+{
+public static void main(String args[])
+{
+Student s= new Student();
+s.show();
+}
+}
