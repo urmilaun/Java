876a48f1e4680c4654922ced8f70a45422132544
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-03 15:20:04 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+public class Pattern1
+{
+public static void main(String args[])
+{
+int n = 4;
+int m = 5;
+for(int i = 1; i<=n; i++)
+{
+for(int j = 1; j<=5; j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
