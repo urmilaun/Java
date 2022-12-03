ba66ffdaa03e297a23ac4a23a8ff1d797fdd39ee
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-03 14:34:23 +0530

    Add files via upload

---
+++ ArithmeticExample.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class ArithmeticExample
+{
+public static void main(String[] args)
+{
+//Addition
+int a =1;
+int b = 2;
+int c= a+b;
+System.out.println(c);
+
+//Substraction
+int a =1;
+int b = 2;
+int c= a-b;
+System.out.println(c);
+
+// Multiplication
+int a =1;
+int b = 2;
+int c= a*b;
+System.out.println(c);
+
+//division
+int a =1;
+int b = 2;
+int c= a/b;
+System.out.println(c);
+
+//Modulo
+int a =1;
+int b = 2;
+int c= a%b;
+System.out.println(c);
+}
+}
---
+++ LogicalExample.java
@@ -0,0 +1,17 @@
+public class LogicalExample
+{
+public static void main(String args[])
+{
+int a =60;
+int b= 40;
+
+//And Operator
+if(a < 50 && b < 50)
+System.out.println("Atleast one less than 50");
+
+//OR opeator
+if(a < 50 || b < 50)
+System.out.println("Atleast one less than 50");
+}
+}
+
