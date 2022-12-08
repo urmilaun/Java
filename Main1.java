25db2255745027673793ce003f6d00067cd47124
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-08 08:44:56 +0530

    Addition of three number

---
+++ Main1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main1{
+    public static void main(String args[])
+    {
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        int sum = a + b + c;
+        
+        System.out.println(sum);
+    }
+}
