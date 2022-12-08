80fd3e7fc772d7aea4442bb9f20e1f5fbe2cca59
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-08 08:52:42 +0530

    Calculate cgpa

---
+++ Main2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main2{
+    public static void main(String args[])
+    {
+       int a = 90;
+       int b = 80;
+       int c = 95;
+       int total = a+b+c;
+       float cgpa = total/100*3;
+       System.out.println(cgpa);
+    }
+}
