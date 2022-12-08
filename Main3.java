2d4cb612d5ad833e5c8692391d451baedd8b9c08
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-08 09:13:28 +0530

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+class Main3{
+    public static void main(String args[])
+    {
+      System.out.println("What is your name?");
+      Scanner sc = new Scanner(System.in);
+      String s = sc.nextLine();
+      System.out.println("Hello My name is Urmila");
+    }
+}
