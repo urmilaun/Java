feff8a451089da21f980b2677fd99addf7012677
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-08 09:14:09 +0530

    Operators in java

---
+++ Main4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+class Main4{
+    public static void main(String args[])
+    {
+      int sum, mul, sub , div;
+      int a = 10, b = 20;
+      //Addition
+      sum = a + b;
+      System.out.println(sum);
+      
+      //substraction
+      sub = a - b;
+      System.out.println(sub);
+      
+      // multiplication
+      mul = a * b;
+      System.out.println(mul);
+      
+      //division
+      div = a / b;
+      System.out.println(div);
+      
+    }
+}
