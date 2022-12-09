2bdd5cc8754aaf1af05a6dcc4f8f526f257d4cd2
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-09 13:08:42 +0530

    Prime Number

---
+++ PrimeNumber.java
@@ -0,0 +1,17 @@
+class PrimeNumber{
+public static void main(String args[]){
+int no = 7;
+for(int i = 2;i<=no-1;i++)
+{
+temp = temp + 1;
+}
+if(temp==0)
+{
+System.out.println(no+"is prime no");
+}
+else
+{
+System.out.println(no+" is not prime");
+}
+}
+}
