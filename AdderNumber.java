8dc670ab7f5ed478282441b87bcbf7a9d5336df2
Urmila <103355024+urmilaun@users.noreply.github.com>
2022-12-19 13:12:13 +0530

    Method Overloading: changing data type of arguments

---
+++ AdderNumber.java
@@ -0,0 +1,15 @@
+class Adder{
+    
+static int add(int a,int b)
+{
+    return a+b;}  
+static int add(int a,int b,int c)
+{
+    return a+b+c;}  
+}  
+class AdderNumber{  
+public static void main(String[] args)
+{  
+System.out.println(Adder.add(11,11));  
+System.out.println(Adder.add(11,11,11));  
+}}  
